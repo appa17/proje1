62ba8a414281b51e8d03fa772cae3aa363d155c7
appa17 <63860683+appa17@users.noreply.github.com>
2024-07-19 21:15:26 +0300

    Add files via upload

---
+++ Denemelik.py
@@ -0,0 +1,13 @@
+print("Arabamı sevdim." + " Ama rengi daha güzel olabilirdi.")
+
+print ("Bele")
+
+print ("123654 - " * 65)
+
+print('NABER \ncano\n:*')
+
+print('Onur','DEMİR')
+
+print('Onur' + 'Demir')
+
+input()
